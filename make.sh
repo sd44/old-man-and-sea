545ddd56874ed76e17c58221479582281497fc3e
sd44 <sd44sd44@yeah.net>
2025-01-06 14:27:52 +0800

    version 1.0alpha

--- make.sh
+++ make.sh
@@ -1,24 +1,24 @@
+#!/bin/sh
+#
+set -e  # 遇到任何命令失败时立即退出脚本
 #
 # 生成词汇注解版老人与海
 #
-cp theoldman_en.tex tmp.tex &&
-latexmk -C theoldman.tex &&
-xelatex theoldman.tex &&
-bib2gls --group theoldman &&
-xelatex theoldman.tex &&
-bib2gls --group theoldman &&
-xelatex theoldman.tex &&
-xelatex theoldman.tex&&
-cp theoldman.pdf 'The Old Man and the Sea 词汇注解版.pdf'&&
+cp theoldman_en.tex tmp.tex 
+latexmk -C theoldman.tex 
+xelatex theoldman.tex 
+bib2gls --group theoldman 
+xelatex theoldman.tex 
+xelatex theoldman.tex
+cp theoldman.pdf 'The Old Man and the Sea 词汇注解版.pdf'
 #
 # 生成双解词汇注解版老人与海
 #
-python combine2file.py &&
-cp theoldman_cn_en.tex tmp.tex&&
-xelatex theoldman.tex &&
-bib2gls --group theoldman &&
-xelatex theoldman.tex &&
-bib2gls --group theoldman &&
-xelatex theoldman.tex &&
-xelatex theoldman.tex
-cp theoldman.pdf 'The Old Man and the Sea 双语 词汇注解版.pdf'
+python combine2file.py 
+cp theoldman_cn_en.tex tmp.tex
+xelatex theoldman.tex 
+bib2gls --group theoldman 
+xelatex theoldman.tex 
+xelatex theoldman.tex 
+cp theoldman.pdf 'The Old Man and the Sea 双语 词汇注解版.pdf' 
+rm theoldman.pdf
