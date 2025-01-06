dcb13d741e720cf5cb90a010885d2d69d1309961
sd44 <sd44sd44@yeah.net>
2025-01-06 12:12:38 +0800

    aaaa

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
+#
+# 生成词汇注解版老人与海
+#
 cp theoldman_en.tex tmp.tex &&
 latexmk -C theoldman.tex &&
 xelatex theoldman.tex &&
@@ -8,11 +11,10 @@ xelatex theoldman.tex &&
 xelatex theoldman.tex&&
 cp theoldman.pdf 'The Old Man and the Sea 词汇注解版.pdf'&&
 #
-# 生成双解版pdf
+# 生成双解词汇注解版老人与海
 #
 python combine2file.py &&
 cp theoldman_cn_en.tex tmp.tex&&
-latexmk -C theoldman.tex &&
 xelatex theoldman.tex &&
 bib2gls --group theoldman &&
 xelatex theoldman.tex &&
